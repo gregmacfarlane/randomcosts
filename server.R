75ec1fd573c87d21055567d1a62cd420aa40b432
Greg Macfarlane <gregmacfarlane@gmail.com>
2025-03-14 08:02:56 -0600

    Add Shiny app for cash flow generation with problem visualization

---
+++ server.R
@@ -0,0 +1,132 @@
+library(shiny)
+library(tidyverse)
+source("R/equivalence_functions.R")
+
+# generate a problem: initial present value, one or more annuities, and betweeen
+# 0 and 3 future values
+generate_problem <- function(seed = 10) {
+
+  set.seed(seed)
+  # generate a random length of project (1 to 10 years)
+  project_length <- sample(1:10, 1)
+  # Generate a random present value (-1000 to -10000)
+  present_value <- sample(1:10, 1) * -1000
+
+  # Generate a random annuity (100 to 1000)
+  annuity <- sample(1:10, 1) * 100
+
+  # Generate a random number of future values (0 to 3)
+  num_future_values <- sample(1:3, 1)
+  # pick random years in project length for the future values
+  future_years <- sample(1:project_length, num_future_values, replace = FALSE)
+  # pick random future values -500 to 500
+  future_values <- sample(c(-5:-1, 1:5), num_future_values) * 100
+
+  futures <- list()
+  for (i in 1:num_future_values) {
+    futures[[future_years[i]]] <- future_values[i]
+  }
+
+  # Generate a random interest rate (0.01 to 0.1)
+  rates <- c(0.005, 0.01, 0.015, 0.02, 0.03, 0.04, 0.05, 0.06, 
+             0.07, 0.08, 0.1, 0.12, 0.18)
+  interest_rate <- sample(rates, 1) * 100
+
+  # Return the problem as a list
+  list(present_value = present_value,
+       annuities = annuity,
+       future_values = futures,
+       interest_rate = interest_rate)
+}
+
+
+# turn the problem into a text problem 
+problem_to_text <- function(problem) {
+  # Extract the values from the problem
+  present_value <- problem$present_value
+  annuities <- problem$annuities
+  future_values <- problem$future_values
+
+  # Create the text problem
+  text_problem <- paste("<h3>Problem</h3><br>You have a present value of", present_value, 
+                        "and an annuity of", annuities, "for", length(future_values), "years.")
+  
+  # Add future values to the text problem
+  future_text <- paste("In the following years, you have future values of: <ul> ")
+  for (year in seq_along(future_values)) {
+    if (!is.null(future_values[[year]])) {
+        future_text <- paste(future_text, "<li>", future_values[[year]], " in year ", year, "</li>")
+    }
+  }
+
+  future_text <- paste(future_text, "</ul>")
+  text_problem <- paste(text_problem, future_text, " \n Find the net present value of this project using a discount rate of ", 
+                        problem$interest_rate, "%.")
+
+  
+  return(text_problem)
+}
+
+
+# turn the problem into a diagram using ggplot
+problem_to_diagram <- function(problem) {
+  # Extract the values from the problem
+  present_value <- problem$present_value
+  annuities <- problem$annuities
+  years <- length(problem$future_values)
+
+  # turn future_values into a vector with zeros for missing values
+  fv <- vector("numeric", length = 1 + years)
+  for(i in 1:years) {
+    if (!is.null(problem$future_values[[i]])) {
+      fv[i + 1] <- problem$future_values[[i]]
+    }
+  }
+
+  # Create a data frame for the cash flows
+  cash_flows <- data.frame(
+    Year = 0:years,
+    Present = c(present_value, rep(0, years)),
+    Annuity = c(0, rep(annuities, years)),
+    Future = fv
+  ) |>
+    dplyr::mutate(CashFlow = Present + Annuity + Future)
+    # pivot_longer(-Year, names_to = "Type", values_to = "CashFlow")
+
+  # Create the plot
+  ggplot2::ggplot(cash_flows, aes(x = Year, y = CashFlow)) +
+    ggplot2::geom_bar(stat = "identity") +
+    ggplot2::geom_text(aes(label = CashFlow), vjust = 0) + 
+    ggplot2::geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+    ggplot2::labs(x = "Year", y = "Cash Flow") +
+    ggplot2::theme_minimal()
+}
+
+
+problem <- generate_problem()
+
+# Define server logic
+shinyServer(function(input, output) {
+  
+  # Generate a plot of the data
+  output$distPlot <- renderPlot({
+    problem_to_diagram(problem)
+  })
+
+  output$problemText <- renderText({
+    problem_to_text(problem)
+  })
+
+
+  output$info <- renderUI({
+    HTML("<p>This is a simple cash flow generator. It generates a random present 
+         value, annuity, and future values.  It also generates a random interest 
+         rate. The cash flows are then plotted on a bar chart. Changing the number
+         below will change the problem that you see. </p>")
+  })
+
+  # Generate text output
+  output$text <- renderText({
+    paste("Cash Flow Diagram", input$title)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,27 @@
+library(shiny)
+# Define UI for application
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Cash Flow Generator"),
+  
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
+    sidebarPanel(
+      htmlOutput("info"),
+    # Input: Numeric entry for seed number
+    numericInput("seed", "Seed Number:", value = 1),
+    actionButton("simulate", "Generate a problem!"),
+      # Output: Text for problem
+      htmlOutput("problemText")
+    ),
+    
+    mainPanel(
+      # Output: Plot
+      plotOutput("distPlot"),
+      
+      # Output: Text
+      textOutput("text")
+    )
+  ))
+)
